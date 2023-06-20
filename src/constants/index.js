3fcf352d4e4614ec6acc63ce53665cfb4e39c3bb
Patrick-Njiru <patricknjiru93@gmail.com>
2023-06-21 02:38:57 +0300

    add images and data.

---
+++ src/constants/index.js
@@ -0,0 +1,115 @@
+import {
+    twenty10s,
+    chill,
+    daydreamer,
+    deepFocus,
+    focusFlow,
+    latino,
+    mix,
+    piano,
+    quiet,
+    rapcaviar,
+    reading,
+    rock,
+    sleep,
+    study,
+    think,
+    topHits,
+    eighties
+} from './images'
+
+export const 
+
+focusMusic = [
+    {
+        musicTitle: 'Peaceful Piano',
+        description: 'Peaceful piano to help you slow down, breathe, and relax.',
+        cover: piano
+    },
+    {
+        musicTitle: 'Deep Focus',
+        description: 'Keep calm and focus with ambient and post-rock music.',
+        cover: deepFocus
+    },
+    {
+        musicTitle: 'Instrumental Study',
+        description: 'Focus with soft study music in the background.',
+        cover: study
+    },
+    {
+        musicTitle: 'Focus Flow',
+        description: 'Uptempo instrumental hip hop beats.',
+        cover: focusFlow
+    },
+    {
+        musicTitle: 'Reading Adventure',
+        description: 'Scores and soundtracks for daring quests, epic journeys, and grand adventures',
+        cover: reading
+    },
+    {
+        musicTitle: 'Beats to think to',
+        description: 'Focus with deep techno and tech house.',
+        cover: think
+    },
+    {
+        musicTitle: 'Quiet Moment',
+        description: 'Gentle classical music to help you relax and reflect',
+        cover: quiet
+    },
+],
+
+playlists = [
+    {
+        playList: "Today's Top Hits",
+        description: 'Doja Cat on top of the hottest 50!',
+        cover: topHits
+    },
+    {
+        playList: 'RapCaviar',
+        description: 'New music from Gunaa, Gucci Mane and Doja Cat.',
+        cover: rapcaviar
+    },
+    {
+        playList: 'All Out 2010s',
+        description: 'The biggest songs of the 2010s',
+        cover: twenty10s
+    },
+    {
+        playList: 'Rock Classics',
+        description: 'Rock legends and epic songs that continue to inspire generations.',
+        cover: rock
+    },
+    {
+        playList: 'Chill Hits',
+        description: 'Kick back to the best new and recent chill hits.',
+        cover: chill
+    },
+    {
+        playList: 'Viva Latino',
+        description: "Today's top Latin hits, elevando nuestra musica. Cover: Don",
+        cover: latino
+    },
+    {
+        playList: 'Mega Hit Mix',
+        description: 'A mega mix of 75 favorites from the last few years',
+        cover: mix
+    },
+    {
+        playList: 'All Out 80s',
+        description: 'The biggest songs in the 1980s',
+        cover: eighties
+    },
+],
+
+sleepMusic = [
+    {
+        instrumental: 'Sleep',
+        description: 'Gentle Ambient piano to help you fall asleep.',
+        cover: sleep
+    },
+    {
+        instrumental: 'DayDreamer',
+        description: 'Drift away enthralling instrumntals',
+        cover: daydreamer
+    },
+]
