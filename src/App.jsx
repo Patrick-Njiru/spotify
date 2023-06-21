ca455b820bbbf3f90b7362b53dbe1e733249a175
Patrick-Njiru <patricknjiru93@gmail.com>
2023-06-21 03:18:52 +0300

    add compoents and containers and add basic routes. Install some packages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import { Home, MusicCategory, SideBar } from './containers'
+
 
 function App() {
 
   return (
-    <div className="">
-        Hello World
-    </div>
+    <Router>
+        <div className="">
+            <SideBar />
+
+            <Routes>
+                <Route exact path='' element={<Home />} />
+
+                <Route path='/category' element={<MusicCategory />} />
+            </Routes>
+        </div>
+    </Router>
   )
 }
 
---
+++ src/containers/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from './Home'
+export { default as MusicCategory } from './Category'
+export { default as SideBar } from './SideBar'
