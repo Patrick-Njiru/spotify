1a45daa3c4def3213ebb4f9f2634285cf7d43ac2
Patrick Njiru <patricknjiru93@gmail.com>
2024-02-27 06:37:24 +0300

    Add the category section.

--- src/scenes/podcasts/Categories.jsx
+++ src/scenes/podcasts/Categories.jsx
@@ -1,7 +1,25 @@
+import { Card } from "../../components"
+import { allPodcastCategories } from "../../data"
+
 const categories = () => {
-  return (
-    <div>categories</div>
-  )
+	console.log(Object.entries(allPodcastCategories))
+	return (
+		<section>
+			<h1 className='text-7xl bg-gradient-to-b from-zinc-800 to-zinc-900 font-extrabold pt-14 pb-6 mb-8 ps-6 '>
+				All podcast categories
+			</h1>
+			{Object.entries(allPodcastCategories).map(([title, podcasts]) => (
+				<div key={title} className=''>
+					<h1 className='ps-6 mt-12 mb-1 text-2xl font-bold'>{title}</h1>
+					<div className='px-6 flex flex-wrap justify-between'>
+						{podcasts.map((podcast, index) => (
+							<Card key={index} podcast={podcast} />
+						))}
+					</div>
+				</div>
+			))}
+		</section>
+	)
 }
 
 export default categories
